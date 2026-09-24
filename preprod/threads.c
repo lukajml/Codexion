84e8ac614dd779acfc3d417dd6c96705fb351bfd
lukajml <lukalaurent@icloud.com>
2026-09-24 16:18:05 +0200

    learning pthread

--- preprod/threads.c
+++ preprod/threads.c
@@ -1,21 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   codexion.h                                         :+:      :+:    :+:   */
+/*   threads.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lulauren <luka.laurent@learner.42.tech>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/24 13:56:14 by lulauren          #+#    #+#             */
-/*   Updated: 2026/09/24 14:08:32 by lulauren         ###   ########.fr       */
+/*   Created: 2026/09/24 14:29:00 by lulauren          #+#    #+#             */
+/*   Updated: 2026/09/24 14:29:11 by lulauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pthread.h>
-#include <stdio.h>
+#include "../headers/codexion.h"
 
-#ifndef CODEXION_H
-#define CODEXION_H
+void*   foo(void* arg)
+{
+    printf("Thread is running\n");
+}
 
+int main()
+{
+    pthread_t   thread;
 
+    pthread_create(&thread, NULL, foo, NULL);
+    pthread_join(thread, NULL);
+}
 
-#endif
---
+++ preprod/threads.md
@@ -0,0 +1,42 @@
+# What are threads?
+
+it's like running several tasks at once, threads are mini programs inside a single process that let you multitask.
+
+    - a process has its own memory space
+    - a thread shares memory space with other threads in the same process
+
+# What is pthread?
+
+pthread is a library in C/C++ that helps create and manage threads.
+functions start with pthread_
+
+## How to create a thread:
+
+```c
+#include <stdio.h>
+#include <pthread.h>
+
+void*   myThreadFunction(void* arg)
+{
+    printf("Hello from the new thread!\n");
+    return NULL;
+}
+
+int main()
+{
+    pthread_t threadId;
+    
+    //Create a new thread
+    pthread_create(&threadId, NULL, myThreadFunction, NULL);
+    
+    //Wait for the thread to finish
+    pthread_join(threadId, NULL);
+    
+    printf("Back in main thread.\n");
+}
+```
+
+### How it works:
+
+ - pthread_create(...) creates a thread and runs myThreadFunction in it.
+ - pthread_join(...) waits until the thread finishes.
